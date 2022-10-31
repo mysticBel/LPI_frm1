3e9e24dd0d94ea8b63466f92d5cc46286efba677
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-31 00:13:43 -0500

    añadiendo validaciones para FrmRegistro.java

--- src/validaciones/Validaciones.java
+++ src/validaciones/Validaciones.java
@@ -3,7 +3,9 @@ package validaciones;
 public class Validaciones {
 	public static final String COD_PRODUCTO ="[pP]\\d{4}";
 	public static final String NOM_PRODUCTO ="[a-zA-Z\\s]{3,20}";
-	
+	public static final String NOM_USUARIO ="[a-zA-Z\\s]{4,20}";
+	public static final String APELLIDO_USUARIO ="[a-zA-Z\\s]{4,35}";
+	public static final String USUARIO ="U[0-9]{3}";  
 	
 	//VALIDACIONES CLIENTE
 	public static final String COD_CLIENTE ="[Cc][Ll]\\d{3}"; //cl001 o CL001
